15036a3ce20223bf67166b7551854931892f0b78
Robbie Wain <robbie.wain@omadahealth.com>
2015-06-14 22:08:43 -0700

    Fix link for Ask HN stories

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -12,14 +12,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	var item = bg.item;
 
 	if (item) {
-		$('#title').text(item.title).attr('href', item.url);
-		if (item.url) {
-			$('#site').text('(' + getHostnameFromURL(item.url) + ')');
+		var discussURL = 'https://news.ycombinator.com/item?id=' + item.id;
+		var url = item.url;
+		if (!url) {
+			url = discussURL;
 		}
+		$('#title').text(item.title).attr('href', url);
+		$('#site').text(item.url ? '(' + getHostnameFromURL(url) + ')' : '');
 		$('#points').text(item.score + ' points');
 		$('#user').text(item.by).attr('href', 'https://news.ycombinator.com/user?id=' + item.by);
-
-		var discussURL = 'https://news.ycombinator.com/item?id=' + item.id;
 		$('#timestamp').text(moment(item.time*1000).fromNow()).attr('href', discussURL);
 		$('#discuss').attr('href', discussURL);
 		$('#comments').text(item.descendants + ' comments').attr('href', discussURL);
